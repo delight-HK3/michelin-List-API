88fd3a7ff81f6ae7b48b8242ea66e8a7682fa39c
leedabean <dabin310153@naver.com>
2024-10-15 10:12:27 +0900

    QueryDSL 조건추가
    QueryDSL 조건추가

--- src/main/java/com/api/michelinAPI/repository/ResultRepository.java
+++ src/main/java/com/api/michelinAPI/repository/ResultRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import com.api.michelinAPI.dto.paramDTO;
 import com.api.michelinAPI.entity.QResultEntity;
 import com.api.michelinAPI.entity.ResultEntity;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import jakarta.persistence.EntityManager;
@@ -18,7 +20,8 @@ public class ResultRepository{
     
     // QueryDSL
     private final JPAQueryFactory queryFactory;
-    
+    private final QResultEntity resultEntity;
+
     // JPQL
     //private final EntityManager em;
     
@@ -26,13 +29,22 @@ public class ResultRepository{
     //List<ResultEntity> searchListBy(@Param("dto") paramDTO dto, String tableNm);
 
     public List<ResultEntity> findMichelinList(paramDTO dto){
-        QResultEntity resultEntity = QResultEntity.resultEntity; 
-        List<ResultEntity> result = queryFactory.from(resultEntity)
-                                    .select(resultEntity)
-                                    .where(resultEntity.starCnt.eq(dto.getStarCnt()))
-                                    .fetch();
-
-        return result;
+        return queryFactory.from(resultEntity) 
+                            .select(resultEntity)
+                            .where(
+                                allSearch(dto)
+                            )
+                            .fetch();
     }
 
+    // 별개수 지정 메서드
+    private BooleanExpression equalStarCnt(paramDTO dto){
+        return dto.getStarCnt() != null ? resultEntity.starCnt.eq(dto.getStarCnt()) : null;
+    }
+
+    // 전체 검색조건
+    private BooleanExpression allSearch(paramDTO dto){
+        return equalStarCnt(dto);
+    }
+    
 }
