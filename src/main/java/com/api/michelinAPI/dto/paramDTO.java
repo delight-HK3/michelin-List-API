74f38b42e5947541a51ae6fddeff65db7c228a30
leebean <dabin310153@naver.com>
2024-11-13 16:54:44 +0900

    불필요한 어노테이션 삭제

--- src/main/java/com/api/michelinAPI/dto/paramDTO.java
+++ src/main/java/com/api/michelinAPI/dto/paramDTO.java
@@ -2,17 +2,11 @@ package com.api.michelinAPI.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성
-@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 생성
 public class paramDTO {
     
     @NotNull
